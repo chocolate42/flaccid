c50318e5cf4aac15920267b32c0d81ba108f19f4
anon <anon>
2023-02-08 14:30:03 +0000

    Have gasc use --blocksize-list instead of --blocksize-limit-lower to define the starting point, as it should be.

--- gasc.c
+++ gasc.c
@@ -12,7 +12,8 @@ int gasc_main(void *input, size_t input_size, FILE *fout, flac_settings *set){
 	queue q;
 	simple_enc *swap, *a, *b, *ab;
 
-	assert(set->blocksize_limit_upper>=(2*set->blocksize_limit_lower));
+	if(set->blocksize_min!=set->blocksize_max)
+		goodbye("Error: gasc cannot use multiple block sizes\n");
 
 	a =calloc(1, sizeof(simple_enc));
 	b =calloc(1, sizeof(simple_enc));
@@ -23,34 +24,34 @@ int gasc_main(void *input, size_t input_size, FILE *fout, flac_settings *set){
 		MD5_Init(&ctx);
 
 	cstart=clock();
-	if(!simple_enc_eof(&q, &a, set, input, &curr_sample, tot_samples, 2*set->blocksize_limit_lower, &stat, &ctx, fout)){//if not eof, init
-		simple_enc_analyse(a , set, input, set->blocksize_limit_lower, 0, &stat, &ctx);
-		simple_enc_analyse(b , set, input, set->blocksize_limit_lower, set->blocksize_limit_lower, &stat, &ctx);
-		simple_enc_analyse(ab, set, input, 2*set->blocksize_limit_lower, 0, &stat, NULL);
+	if(!simple_enc_eof(&q, &a, set, input, &curr_sample, tot_samples, 2*set->blocks[0], &stat, &ctx, fout)){//if not eof, init
+		simple_enc_analyse(a , set, input, set->blocks[0], 0, &stat, &ctx);
+		simple_enc_analyse(b , set, input, set->blocks[0], set->blocks[0], &stat, &ctx);
+		simple_enc_analyse(ab, set, input, 2*set->blocks[0], 0, &stat, NULL);
 	}
 
 	while(curr_sample<tot_samples){
 		if((a->outbuf_size+b->outbuf_size)<ab->outbuf_size){//dump a naturally
 			a=simple_enc_out(&q, a, set, input, &curr_sample, &stat, fout);
-			if(!simple_enc_eof(&q, &a, set, input, &curr_sample, tot_samples, 2*set->blocksize_limit_lower, &stat, NULL, fout)){//if next !eof, iterate
+			if(!simple_enc_eof(&q, &a, set, input, &curr_sample, tot_samples, 2*set->blocks[0], &stat, NULL, fout)){//if next !eof, iterate
 				swap=a;
 				a=b;
 				b=swap;
-				simple_enc_analyse(b, set, input, set->blocksize_limit_lower, curr_sample+set->blocksize_limit_lower, &stat, &ctx);
-				simple_enc_analyse(ab, set, input, 2*set->blocksize_limit_lower, curr_sample, &stat, NULL);
+				simple_enc_analyse(b, set, input, set->blocks[0], curr_sample+set->blocks[0], &stat, &ctx);
+				simple_enc_analyse(ab, set, input, 2*set->blocks[0], curr_sample, &stat, NULL);
 			}
 			else if(set->md5)//have to manually finish md5 as eof came at an awkward time
-				MD5_UpdateSamples(&ctx, input, curr_sample-(curr_sample%set->blocksize_limit_lower), (curr_sample%set->blocksize_limit_lower), set);
+				MD5_UpdateSamples(&ctx, input, curr_sample-(curr_sample%set->blocks[0]), (curr_sample%set->blocks[0]), set);
 		}
-		else if(ab->sample_cnt+set->blocksize_limit_lower>set->blocksize_limit_upper){//dump ab as hit upper limit
+		else if(ab->sample_cnt+set->blocks[0]>set->blocksize_limit_upper){//dump ab as hit upper limit
 			ab=simple_enc_out(&q, ab, set, input, &curr_sample, &stat, fout);
-			if(!simple_enc_eof(&q, &a, set, input, &curr_sample, tot_samples, 2*set->blocksize_limit_lower, &stat, &ctx, fout)){//if next !eof, iterate
-				simple_enc_analyse(a, set, input, set->blocksize_limit_lower, curr_sample, &stat, &ctx);
-				simple_enc_analyse(b, set, input, set->blocksize_limit_lower, curr_sample+set->blocksize_limit_lower, &stat, &ctx);
-				simple_enc_analyse(ab, set, input, 2*set->blocksize_limit_lower, curr_sample, &stat, NULL);
+			if(!simple_enc_eof(&q, &a, set, input, &curr_sample, tot_samples, 2*set->blocks[0], &stat, &ctx, fout)){//if next !eof, iterate
+				simple_enc_analyse(a, set, input, set->blocks[0], curr_sample, &stat, &ctx);
+				simple_enc_analyse(b, set, input, set->blocks[0], curr_sample+set->blocks[0], &stat, &ctx);
+				simple_enc_analyse(ab, set, input, 2*set->blocks[0], curr_sample, &stat, NULL);
 			}
 		}
-		else if(curr_sample+ab->sample_cnt+set->blocksize_limit_lower>tot_samples){//hit eof in middle of frame
+		else if(curr_sample+ab->sample_cnt+set->blocks[0]>tot_samples){//hit eof in middle of frame
 			if(set->md5)
 				MD5_UpdateSamples(&ctx, input, curr_sample+ab->sample_cnt, (tot_samples-(curr_sample+ab->sample_cnt)), set);
 			if(!simple_enc_eof(&q, &a, set, input, &curr_sample, tot_samples, tot_samples, &stat, NULL, fout))
@@ -60,8 +61,8 @@ int gasc_main(void *input, size_t input_size, FILE *fout, flac_settings *set){
 			swap=a;
 			a=ab;
 			ab=swap;
-			simple_enc_analyse(b, set, input, set->blocksize_limit_lower, curr_sample+a->sample_cnt, &stat, &ctx);
-			simple_enc_analyse(ab, set, input, a->sample_cnt+set->blocksize_limit_lower, curr_sample, &stat, NULL);
+			simple_enc_analyse(b, set, input, set->blocks[0], curr_sample+a->sample_cnt, &stat, &ctx);
+			simple_enc_analyse(ab, set, input, a->sample_cnt+set->blocks[0], curr_sample, &stat, NULL);
 		}
 	}
 	queue_dealloc(&q, set, input, &stat, fout);
